2a21c98adb5d61c545b2bf28f1592c7e21148b2b
coldferrin <ferrinkatz@ferrinkatz.com>
2018-04-23 15:41:02 -0700

    Added comp key unit tests and started work on display web page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,118 @@
-$(Document).ready(function(){
+var documentReady = false;
+var $content = $("#content");
+var $charts = $("#charts");
+var $submitButton = $("#btn1");
+var $sensorSelect = $("#sensor_Select");
+var charts = [];
+
+var LineChart;
+// noinspection BadExpressionStatementJS
+LineChart = {
+    ctx:"",
+    chart:"",
+
+    getLength: function () {
+        return this.chart.datasets.length;
+    },
+
+    addData: function (label, data) {
+        this.chart.data.labels.push(label);
+        for(var i = 0; i < this.chart.data.datasets.length; i++){
+            this.chart.data.datasets[i].data.push(data);
+        }
+
+        this.chart.update();
+    },
+
+    removeData: function () {
+        this.chart.data.labels.shift();
+        for(var i = 0; i < this.chart.data.datasets.length; i++){
+            this.chart.data.datasets[i].data.shift();
+        }
+        this.chart.update();
+    },
+
+    updateChart: function (label, data) {
+        this.removeData();
+        this.addData(label, data);
+    },
+
+    buttonFunction: function () {
+        console.info("in buttonFunction");
+
+        this.chart = new Chart(this.ctx, {
+            type: 'line',
+            data: {
+                datasets: [{
+                    label: 'Sensor Data',
+                    data: [],
+                    backgroundColor: [
+                        'rgba(0, 0, 255, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(0,0,255,1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+};
+
+var data = 1;
+function addData(ac) {
+    data = data + Math.round(5 + Math.random() * (-5 - 5));
+
+    if (ac.chart.data.datasets[0].data.length > 30) {
+        ac.updateChart(Date.now(), data);
+    }
+    else{
+        ac.addData(Date.now(), data);
+    }
+}
+
+$(document).ready(function() {
+    console.info("Page Loaded");
+    $charts.hide()
+
+    var chart1 = Object.create(LineChart);
+    chart1.ctx =  $("#chart1");
+
+    charts.push(chart1);
+
+    var chart2 = Object.create(LineChart);
+    chart2.ctx = $("#chart2");
+
+    charts.push(chart2)
+
+    charts[0].buttonFunction();
+
+    charts[1].buttonFunction();
+
+    documentReady = true;
+
 
 });
+
+$submitButton.on('click', function (e) {
+    if(documentReady) {
+        $charts.show();
+        $sensorSelect.hide();
+
+        setInterval(function () {
+            addData(charts[0])
+        }, 1000);
+        setInterval(function () {
+            addData(charts[1])
+        }, 1000);
+    }
+})
--- src/test/java/org/erau/eas/serverweb/db/CompKeys/ConfigKeyTest.java
+++ src/test/java/org/erau/eas/serverweb/db/CompKeys/ConfigKeyTest.java
@@ -1,4 +1,32 @@
 package org.erau.eas.serverweb.db.CompKeys;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ConfigKeyTest {
+    @Test
+    public void getSetTest(){
+        ConfigKey configKey = new ConfigKey(1,1,1);
+        assertEquals(1, configKey.getFlightID());
+        assertEquals(1, configKey.getBoardId());
+        assertEquals(1, configKey.getSensorId());
+    }
+
+    @Test
+    public void hashCodeTest(){
+        ConfigKey configKey = new ConfigKey(1,1,1);
+        assertEquals(993,configKey.hashCode());
+    }
+
+    @Test
+    public void equalsTest(){
+        ConfigKey configKey1 = new ConfigKey(1,1,1);
+        ConfigKey configKey2 = new ConfigKey(1,1,1);
+        ConfigKey configKey3 = new ConfigKey(2,2,2);
+
+        assertFalse(configKey1.equals(configKey3));
+        assertTrue(configKey1.equals(configKey2));
+    }
+
+
 }
