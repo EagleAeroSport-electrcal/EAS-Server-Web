e0ba196ef714c9a5ab892e434e75cc708bfeef56
ColdFerrin <ferrinkatz@ferrinkatz.com>
2018-04-24 00:58:11 -0700

    Wrote unit test for boardIdentity

--- src/test/java/org/erau/eas/serverweb/db/BoardIdentityTest.java
+++ src/test/java/org/erau/eas/serverweb/db/BoardIdentityTest.java
@@ -1,4 +1,12 @@
 package org.erau.eas.serverweb.db;
 
+import org.junit.Test;
+
 public class BoardIdentityTest {
+    @Test
+    public void getSetTest(){
+        BoardIdentity boardIdentity = new BoardIdentity();
+        boardIdentity.setId(1);
+        boardIdentity.setMacAddress("F85971535746");
+    }
 }
